f8b925d4152e17107c3b8b83150001434e89ade2
山泉水 <12871638+one-hundred-summer@user.noreply.gitee.com>
2025-05-20 16:54:44 +0800

    搭建一下智能对话，尚未实现

---
+++ backend/AITalk.py
@@ -0,0 +1,6 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.chat_models import ChatZhipuAI
+from langserve import add_routes
+
+
--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,7 @@ HOSTNAME = "127.0.0.1"
 PORT = "3306"
 DATABASE = "flask"
 USERNAME = "root"
-PASSWORD = "srx19980201"# 注意更改
+PASSWORD = "123456"# 注意更改
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
